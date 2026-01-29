110f016c4eb71be4bd5b7aaf174b0f1a2f2d52e9
giorgosskoul <george.larisa@gmail.com>
2026-01-29 13:31:37 +0100

    Updated 2 other utils

--- utils/plotting.py
+++ utils/plotting.py
@@ -1,35 +1,38 @@
 from pathlib import Path
 from typing import Union
+
 import matplotlib.pyplot as plt
 
 
 def plot_losses_and_accuracies(
-    inner_losses_dict, 
-    outer_losses_dict, 
-    acc_training_list, 
-    average_acc_diff, 
-    kmeans_acc,
-    output_dir: Union[str, Path]
+    inner_losses_dict: dict[str, list[float]],
+    outer_losses_dict: dict[str, list[float]],
+    acc_training_list: list[float],
+    average_acc_diff: list[float],
+    kmeans_acc: float,
+    output_dir: Union[str, Path],
 ):
-    """Plot training losses and accuracies.
-    
-    Args:
-        output_dir: Directory to save plots (will create 'plots/' subdirectory)
+    """
+    Plot training losses and accuracies.
+
+    :param inner_losses_dict: Dictionary mapping dataset names to inner loss lists.
+    :param outer_losses_dict: Dictionary mapping dataset names to outer loss lists.
+    :param acc_training_list: List of training accuracies.
+    :param average_acc_diff: List of average accuracy differences.
+    :param kmeans_acc: KMeans accuracy value.
+    :param output_dir: Directory to save plots (will create 'plots/' subdirectory)
     """
     output_dir = Path(output_dir)
     plots_dir = output_dir / "plots"
     plots_dir.mkdir(parents=True, exist_ok=True)
-    
+
     plt.figure()
     for dataset_name in inner_losses_dict:
-        plt.plot(
-            inner_losses_dict[dataset_name],
-            label=f"{dataset_name} - inner loss"
-        )
+        plt.plot(inner_losses_dict[dataset_name], label=f"{dataset_name} - inner loss")
         plt.plot(
             outer_losses_dict[dataset_name],
             linestyle="--",
-            label=f"{dataset_name} - outer loss"
+            label=f"{dataset_name} - outer loss",
         )
 
     plt.xlabel("Iteration")
